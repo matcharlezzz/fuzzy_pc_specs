a33c4e6aad06abbebb30173d0d52d27bd52870cb
jp-jp-123 <jpbeltran098@gmail.com>
2024-07-11 02:20:34 +0800

    added toggable fuzzified value adjustment

--- defuzzifier.py
+++ defuzzifier.py
@@ -1,9 +1,18 @@
 #   activation is fuzzy conjunction of all rules/ membership degree of all rules
 #   output_sets is the fuzzy disjunction/ maximum value of all activation in output membership
 
-def aggregate(ruleset, output_sets, inputs):
+def aggregate(ruleset, output_sets, inputs, enable_weights=True):
+    in_middle = 0
+
     for rule in ruleset:
         inp_budget, inp_workload, inp_storage, out_set = rule
+        if enable_weights:
+            if inputs['budget'][inp_budget][0] == 0.5 and in_middle == 0:
+                inputs['budget'][inp_budget][0] += 0.3
+                in_middle += 1
+            elif inputs['budget'][inp_budget][0] == 0.5 and in_middle > 0:
+                inputs['budget'][inp_budget][0] -= 0.35
+
         activation = min(inputs['budget'][inp_budget], inputs['workload'][inp_workload], inputs['storage'][inp_storage])
         output_sets[out_set] = max(output_sets[out_set], activation[0])
 
