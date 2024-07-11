0876a053047bb066ef0de01469abb24ae96761f8
jp-jp-123 <jpbeltran098@gmail.com>
2024-07-12 01:10:50 +0800

    app testing

---
+++ EZPC-app/app/index.tsx
@@ -0,0 +1,25 @@
+import { Text, View } from "react-native";
+import Slider from '@react-native-community/slider';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>ok nigga</Text>
+
+      <Slider
+        style={{width: 200, height: 40}}
+        minimumValue={0}
+        maximumValue={1}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#000000"
+      />
+    </View>
+  );
+}
