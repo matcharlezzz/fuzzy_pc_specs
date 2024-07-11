a8fb1fac4c0eb647b89e009312302baa748568d3
jp-jp-123 <jpbeltran098@gmail.com>
2024-07-11 23:41:00 +0800

    pretty-fied recommendation output

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from membership import PCSpecsMembership
 import inference_engine as ig
 import defuzzifier as dfz
 import recommender as rcm
+import toJSON
 import pprint
 
 pc = PCSpecsMembership()
@@ -9,21 +10,22 @@ pc = PCSpecsMembership()
 
 val = input("Type number for Budget: ")
 val1 = pc.budget(float(val))
-print(val1)
+# print(val1)
 val = input("Type number for Workload: ")
 val2 = pc.workload(float(val))
-print(val2)
+# print(val2)
 val = input("Type number for Storage: ")
 val3 = pc.storage(float(val))
-print(val3)
+# print(val3)
 
 
 all_inputs = ig.input_combiner(val1, val2, val3)
 aggregate = dfz.aggregate(ig.ruleset, ig.output_sets, all_inputs, False)
-print(aggregate)
 defuzz_out = dfz.defuzzy(aggregate, ig.output_xmid)
 
 print(f'\nFinal PC Score: {defuzz_out}')
 
 recos = rcm.recommendation(defuzz_out)
 pprint.pprint(recos)
+
+toJSON.pack_dict(recos)
--- recommender.py
+++ recommender.py
@@ -1,7 +1,7 @@
 from itertools import islice
 import json
 
-f = open('db/backup-1720626377.json')
+f = open('db/final_charnotate.json')
 
 data = json.load(f)
 
@@ -11,12 +11,31 @@ reco_specs = {}
 def recommendation(score: int) -> dict:
     for i in range(len(data)):
         if data[i]['total_price'] <= score * 10000:
-            reco_specs[data[i]['total_price'] / 10000] = data[i]
+            reco_specs[data[i]['total_price'] / 10000] = {key.strip('()'): value for key, value in data[i].items()}
+            reco_specs[data[i]['total_price'] / 10000].pop('total_price')
 
     sorted_reco = dict(sorted(reco_specs.items(), reverse=True))
     top_five = dict(islice(sorted_reco.items(), 5))
 
-    return top_five
+    pretty_top_five = pretty_format(top_five)
+
+    return pretty_top_five
+
+
+def pretty_format(build_list: dict) -> dict:
+    prettified_build_list = {}
+    build_rank = 1
+
+    for key in build_list:
+        # create a new key with its rank
+        prettified_build_list[build_rank] = {}
+        for components in build_list[key]:
+            # remove {name: } and replace the component value into actual name
+            prettified_build_list[build_rank][components] = build_list[key][components]['name']
+
+        build_rank += 1
+
+    return prettified_build_list
 
 
 f.close()
