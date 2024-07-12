253d6e2ee625715607d300311f952256baf498c2
recoder99 <30973326+recoder99@users.noreply.github.com>
2024-07-12 22:33:22 +0800

    main_router updated

--- main_router.py
+++ main_router.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask import request
 from membership import PCSpecsMembership
+import main as fuzzy
 import inference_engine as ig
 import defuzzifier as dfz
 import recommender as rcm
@@ -23,18 +24,8 @@ def receive_query():
         val2 = pc.workload(float(data["workload"]))
         val3 = pc.storage(float(data["storage"]))
 
-        print(f"val1: {val1}, val2: {val2}, val3: {val3}")
+        response = jsonify(fuzzy.ezpc(val1, val2, val3))
 
-        all_inputs = ig.input_combiner(val1, val2, val3)
-        aggregate = dfz.aggregate(ig.ruleset, ig.output_sets, all_inputs, False)
-        defuzz_out = dfz.defuzzy(aggregate, ig.output_xmid)
-
-        print(f'\nFinal PC Score: {defuzz_out}')
-
-        recos = rcm.recommendation(defuzz_out)
-
-        data = json.dumps(recos, indent=4)
-        response = jsonify(recos)
-        print(response.data)
         return response
+        
 
